a5d10098096bedbb81616997a03dc45ee08ac8a9
EC2 Default User <ec2-user@ip-172-31-16-113.ec2.internal>
2025-10-03 21:01:37 +0000

    error fix to the app

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3001,
+    host: '0.0.0.0',
     proxy: {
       '/api': 'http://localhost:3000',
       '/get-trivia': 'http://localhost:3000',
